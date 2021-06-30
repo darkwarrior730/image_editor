9646928579c80f92a8997611d142b66fa0b04fc9
James Perry <jperry.marlow@gmail.com>
2021-06-30 16:24:46 +0100

    Boilerplate code to generate a window

--- src/app.cpp
+++ src/app.cpp
@@ -4,7 +4,71 @@
 
 #include "shader_s.h"
 
+int WIDTH = 640;
+int HEIGHT = 480;
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    WIDTH = width;
+    HEIGHT = height;
+    glViewport(0, 0, width, height);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (action == GLFW_PRESS) {
+        if (key == GLFW_KEY_ESCAPE) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+    }
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+}
+
 int main(int argc, char *argv[])
 {
+    if (!glfwInit()) {
+        return 1;
+    }
+
+    glfwSetErrorCallback(error_callback);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "My Title", NULL, NULL);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialise GLAD" << std::endl;
+        return 1;
+    }
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
